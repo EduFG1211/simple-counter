35c0d64edbf5e1630da3c5a2f76ac9604365bd33
Edventures <edufg13@gmail.com>
2021-03-05 02:21:49 +0000

    terminado v1

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,42 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import PropTypes from "prop-types";
 
 //create your first component
-export function Home() {
+
+export function SecondsCounter() {
+	const [counter, setCounter] = React.useState(0);
+
+	React.useEffect(() => {
+		const timer =
+			counter >= 0 && setInterval(() => setCounter(counter + 1), 1000);
+		return () => clearInterval(timer);
+	}, [counter]);
+
+	let hundredThousandth =
+		Math.floor(counter / 100000) - Math.floor(counter / 1000000) * 10;
+	let tenThousandth =
+		Math.floor(counter / 10000) - Math.floor(counter / 100000) * 10;
+	let thousandth =
+		Math.floor(counter / 1000) - Math.floor(counter / 10000) * 10;
+	let hundredth = Math.floor(counter / 100) - Math.floor(counter / 1000) * 10;
+	let tenth = Math.floor(counter / 10) - Math.floor(counter / 100) * 10;
+	let units = counter - Math.floor(counter / 10) * 10;
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="bigCounter">
+			<div className="calendar">
+				<i className="far fa-clock"></i>
+			</div>
+			<div>{hundredThousandth}</div>
+			<div>{tenThousandth}</div>
+			<div>{thousandth}</div>
+			<div>{hundredth}</div>
+			<div>{tenth}</div>
+			<div>{units}</div>
 		</div>
 	);
 }
+
+SecondsCounter.propTypes = {
+	second: PropTypes.number
+};
